1f975f15325b1d40dd5e86ad8816d2a270e31012
bloxarynid <bloxaryn.id@gmail.com>
2026-01-08 21:10:11 +0700

    Update kegiatan.js

--- information/kegiatan/kegiatan.js
+++ information/kegiatan/kegiatan.js
@@ -5,6 +5,22 @@ window.kegiatanData = [
         date: "20 Oktober 2025",
         lokasi: "Sekolah",
         content: "Memperingati Hari Santri secara Nasional dengan memakai baju bernuansa (serba) putih."
+    },
+    
+    {
+        title: "Kegiatan Evaluasi Semester Ganjil dan Persiapan Pemilihan Ketua Osis",
+        date: "9 Januari 2026",
+        lokasi: "Indoor, Esphero",content: '<p><em>Assalamualaikum wrwb</em></p>\n\n' +
+'<p>Menginformasikan untuk kegiatan jumpa pakar hari Jum\'at, 9 Januari 2026 adalah <strong>evaluasi kegiatan semester ganjil dan persiapan pilketos</strong> dg ketentuan sebagai berikut:</p>\n\n' + // ← Perhatikan \' di "Jum'at"
+'<ol>\n' +
+'    <li>Setelah bel jam 07.00 Wib seluruh <em>siswa menuju ke indoor</em></li>\n' +
+'    <li>Sholat Jum\'at dimasjid luar sekolah.</li>\n' + // ← Perhatikan \' di "Jum'at"
+'    <li>Keputrian ditiadakan</li>\n' +
+'    <li>Siswa/i berseragam Pramuka lengkap.</li>\n' +
+'</ol>\n\n' +
+'<p>Demikian informasi ini disampaikan, terimakasih.</p>\n\n' +
+'<p><em>Wassalamu\'alaikum wrwb.</em></p>' // ← Perhatikan \' di "Wassalamu'alaikum"
+        
     }
 ];
 
